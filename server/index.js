34fd0abd6cde4ac6df01b15010a7f07aff0adcff
yonatana <yonatana@jfrog.com>
2024-07-23 16:32:25 +0300

    added server static

--- server/index.js
+++ server/index.js
@@ -1,9 +1,21 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 const port = 3000;
-
-app.get('/ping', (req, res) => {
+const staticFilesPath = path.join(__dirname, '../dist');
+app.use(express.static(staticFilesPath));
+app.get('/api/ping', (req, res) => {
   res.send('Pong!');
+});
+
+
+app.get('/', (req, res) => {
+  const filePath = path.join(__dirname, '../dist/index.html');
+  res.sendFile(filePath);
 });
 
 app.listen(port, () => {
