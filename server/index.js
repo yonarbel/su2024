0014e6c5cac8e4fad1964449a066e3360bab8375
yonatana <yonatana@jfrog.com>
2024-07-24 10:52:37 +0300

    added unsafe code

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
+const key = 'sgAFGakaZs212';
+const token = 'eyJhbG';
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
@@ -9,9 +12,27 @@ const port = 3000;
 const staticFilesPath = path.join(__dirname, '../dist');
 app.use(express.static(staticFilesPath));
 app.get('/api/ping', (req, res) => {
-  res.send('Pong!');
+  res.send(token);
 });
 
+app.get('/api/user', (req, res) => {
+  // Get the user endpoint from the request query parameters
+  const userEndpoint = req.query.endpoint;
+
+  // Generate the file path based on the user endpoint
+  const filePath = path.join(__dirname, `../dist/${userEndpoint}`);
+
+  // Check if the file exists
+  fs.access(filePath, fs.constants.F_OK, (err) => {
+    if (err) {
+      // If the file doesn't exist, return a 404 status code
+      res.status(404).send('File not found');
+    } else {
+      // If the file exists, send it to the client
+      res.sendFile(filePath);
+    }
+  });
+});
 
 app.get('/', (req, res) => {
   const filePath = path.join(__dirname, '../dist/index.html');
